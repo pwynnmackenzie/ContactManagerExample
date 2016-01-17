e5e0ed313671ca84064f7d902734b0de69e6d810
Paul Wynn-Mackenzie <pwynnmackenzie@gmail.com>
2016-01-17 18:44:47 +0000

    Initial commit, started project

---
+++ ContactManagerExample/ContactManagerExample/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ContactManagerExample
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
